dd3fa56cb841fde7ab539c38b75a3ae8aa285506
Александр Гвоздецкий <hvozdetskyi.alex@gmail.com>
2021-12-27 16:04:59 +0200

    +

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,10 +2,12 @@ const signup = require('./signup');
 const login = require('./login');
 const logout = require('./logout');
 const current = require('./current');
+const addAvatar = require('./addAvatar');
 
 module.exports = {
     login,
     signup,
     logout,
-    current
+    current,
+    addAvatar
 }
